ad9b9394cdebe326562228a494812d30f736d402
Cristian Espindola <cris.esroj@gmail.com>
2019-05-03 23:12:31 -0300

    modifico SetUsuario

--- Practica4/SetUsuario.hs
+++ Practica4/SetUsuario.hs
@@ -3,19 +3,23 @@ data Arbol a =  Nodo a (Arbol a) (Arbol a) | Vacio deriving (Eq, Show)
 
 losQuePertenecen :: Eq a => [a] -> Set a -> [a]
 losQuePertenecen [] cs = []
-losQuePertenecen (x:xs) cs = if (perteneceC x cs)	
+losQuePertenecen (x:xs) cs = if (belongs x cs)	
 									then x : (losQuePertenecen xs cs)
 									else losQuePertenecen xs cs
 									
 sinRepetidos :: Eq a => [a] -> [a]
-sinRepetidos xs = listaC (conjuntoSinRepetidos xs)
+sinRepetidos xs = setToList (conjuntoSinRepetidos xs)
 
 conjuntoSinRepetidos :: Eq a => [a] -> Set a
-conjuntoSinRepetidos [] = vacioC
+conjuntoSinRepetidos [] = emptyS
 conjuntoSinRepetidos (x:xs) = if (elem x xs)
 								then conjuntoSinRepetidos xs
-								else agregarC x (conjuntoSinRepetidos xs)
+								else addS x (conjuntoSinRepetidos xs)
 
 unirTodos :: Eq a => Arbol (Set a) -> Set a
-unirTodos Vacio = vacioC 
-unirTodos (Nodo x ti td) = addS x (unionC (unirTodos ti)(unirTodos td))
+unirTodos Vacio = emptyS 
+unirTodos (Nodo x ti td) = agregar (setToList x) (unionS (unirTodos ti) (unirTodos td))
+
+agregar :: Eq a => [a] -> Set a -> Set a
+agregar [] s 	 = s
+agregar (x:xs) s = addS x (agregar xs s)
